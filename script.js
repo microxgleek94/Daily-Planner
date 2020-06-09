a09de5e79055b0daee8ca0cef56a1cc495fe6dfc
Keiana Hanson <kiki_hanson@aol.com>
2020-06-09 00:28:20 -0400

    intial push

---
+++ script.js
@@ -0,0 +1,6 @@
+//used to put id 
+
+// var description = $("<div>");
+//         $(description).attr("class", "description col-10");
+//         var descriptionRow = $("<div>").attr({ class: "row", id: "description" + (i + 1) });
+//         // $(description).append(descriptionRow);
